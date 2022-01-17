81094b64a392daeda230174c2a4ef780563ca65f
Cristian <cristian.christensen@gmail.com>
2022-01-17 18:22:39 +0100

    User story 1: test environment

---
+++ lib/space.rb
@@ -0,0 +1,24 @@
+require 'pg'
+
+class Space
+
+  def self.all
+    if ENV['RACK_ENV'] == 'test'
+      con = PG.connect :dbname => 'makersbnb_test'
+    else
+      con = PG.connect :dbname => 'makersbnb'
+    end
+    result = con.exec "SELECT * FROM spaces"
+    result.map { |space| space['name']}
+  end 
+
+  def self.create(url:)
+    if ENV['RACK_ENV'] == 'test'
+      con = PG.connect :dbname => 'makersbnb_test'
+    else
+      con = PG.connect :dbname => 'makersbnb'
+    end
+  
+    con.exec("INSERT INTO spaces (name) VALUES('#{name}')")
+  end
+end
