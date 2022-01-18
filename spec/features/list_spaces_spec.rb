2b826896bf7e98db29e1179a381c91054f144030
samir <s-malik@live.co.uk>
2022-01-18 15:20:32 +0000

    Updated Feature test - adding a new space. Test - RED

--- spec/features/list_spaces_spec.rb
+++ spec/features/list_spaces_spec.rb
@@ -3,6 +3,8 @@ feature 'Adding a new space' do
     visit('/spaces/new')
 
     fill_in 'name', with: 'Fuzzy Mountain BnB'
+    fill_in 'date_from', with: 18-01-2022
+    fill_in 'date_to', with: 18-12-2022
     click_on 'Submit'
 
     expect(page).to have_content 'Fuzzy Mountain BnB'
--- spec/space_spec.rb
+++ spec/space_spec.rb
@@ -4,14 +4,10 @@ describe Space do
   describe '.all' do
     it 'returns all spaces' do
       connection = PG.connect(dbname: 'makersbnb_test')
-
       connection.exec("INSERT INTO spaces (name) VALUES ('San Francisco Apartment');")
-      connection.exec("INSERT INTO spaces (name) VALUES ('London House');")
-      
       spaces = Space.all
+      expect(spaces.first.name).to include("San Francisco Apartment")
 
-      expect(spaces).to include("San Francisco Apartment")
-      expect(spaces).to include("London House")
     end
   end
 
@@ -20,7 +16,8 @@ describe Space do
       connection = PG.connect(dbname: 'makersbnb_test')
       connection.exec("INSERT INTO spaces (date_from, date_to) VALUES ('18-01-22','18-12-22');")
       spaces = Space.all
-      expect(spaces).to include('18-01-22','18-12-22')
+      expect(spaces.first.date_from).to eq '18-01-22'
+      expect(spaces.first.date_to).to eq '18-12-22'
     end
   end 
 
