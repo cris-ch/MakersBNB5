d6c0dbbf8d4f320ceb39ce13e9d8d9012c331d44
izaowl <izasow@yahoo.com>
2022-01-19 18:18:34 +0000

    Adding creation of table for US5

--- db/migrations/01_create_space_table.sql
+++ db/migrations/01_create_space_table.sql
@@ -1,2 +1,2 @@
 CREATE TABLE spaces(id SERIAL PRIMARY KEY, name VARCHAR(100), short_description VARCHAR(200), price MONEY, date_from VARCHAR(12), date_to VARCHAR(12);)
--- adding comment
+CREATE TABLE booking(booking_id SERIAL PRIMARY KEY, property_name VARCHAR(100), stay_date VARCHAR(10) );
